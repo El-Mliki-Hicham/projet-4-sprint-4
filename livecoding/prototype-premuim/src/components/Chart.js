6fe30697273ab803d2510745611019dd1bca13bd
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-12-01 00:23:53 -0800

    db

--- livecoding/prototype-premuim/src/components/Chart.js
+++ livecoding/prototype-premuim/src/components/Chart.js
@@ -3,22 +3,20 @@ import { Bar } from "react-chartjs-2";
 import { Chart as ChartJs } from "chart.js/auto";
 
  class Chart extends React.Component{
-state={
-    labels:this.props.DataProps.map((value)=>value.Task),
-
-    datasets:[{
-        label:'dure de tache par /h',
-        data:this.props.DataProps.map((value)=>value.Period) , 
-        backgroundColor:['red'],
-        indexAxis:'y'
-    }]
-}
-
     render(){
         console.log(this.props.DataProps)
         return(
             <div style={{width:700}}>
-            <Bar   data={this.state}/>
+            <Bar   data={{
+                  labels:this.props.DataProps.map((value)=>value.Task),
+
+                  datasets:[{
+                      label:'dure de tache par /h',
+                      data:this.props.DataProps.map((value)=>value.Period) , 
+                      backgroundColor:['red'],
+                      indexAxis:'y'
+                  }]
+            }}/>
         </div>
 
         )
