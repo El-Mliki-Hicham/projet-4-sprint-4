876f855b70c045240febc6284f4ca52ec85e0406
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-12-01 04:21:29 -0800

    data base

--- DB/database/migrations/2022_08_31_192902_create_Formateur.php
+++ DB/database/migrations/2022_08_31_192902_create_Formateur.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('Formateur', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("Nom_formateur")->nullable();
+            $table->string("Prenom_formateur")->nullable();
+            $table->string("Image")->nullable();
+
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('briefs');
     }
 };
---
+++ DB/database/migrations/2022_10_29_134206_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Apprenant', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("Nom")->nullable();
+            $table->string("Prenom")->nullable();
+            $table->string("Email")->nullable();
+            $table->string("Image")->nullable();
+            // $table->unsignedInteger("promotion_id")->nullable();
+            // $table->foreign("promotion_id")
+            // ->references("id")
+            // ->on('promotions')
+            // ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
