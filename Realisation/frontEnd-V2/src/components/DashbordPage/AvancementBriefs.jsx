9540d1b098dfc0e2c21384021b67a756a03e0577
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-12-14 02:20:31 -0800

    realisation v2

---
+++ Realisation/frontEnd-V2/src/components/DashbordPage/AvancementBriefs.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import QuickChart from "quickchart-js";
+import { useState,useEffect } from "react";
+function AvancementBriefs(){
+
+const [AllBriefs,setAllBriefs]=useState([]);
+const cookies = new Cookies()
+
+useEffect(() => {
+    let idFormateur = cookies.get("FormateurID");
+
+
+    const AvancementBrief = async () => {
+        await axios
+          .get("http://localhost:8000/api/OneGroupe/" + idFormateur)
+          .then((res) => {           
+            setAllBriefs(res.data[4]);
+          });
+      };
+      AvancementBrief();
+
+}, []);
+
+
+const ChartBrifes = new QuickChart();
+
+ChartBrifes.setConfig({
+  type: "progressBar",
+  data: {
+    datasets: [
+      {
+        data: AllBriefs.map((value) => value.Percentage),
+        backgroundColor: "green",
+      },
+    ],
+  },
+
+  options: {
+    plugins: {
+      datalabels: {
+        formatter: (val) => {
+          return val.toLocaleString() + "%";
+        },
+
+        font: {
+          size: 30,
+        },
+        color: (context) =>
+          context.dataset.data[context.dataIndex] > 15 ? "#fff" : "#000",
+        anchor: (context) =>
+          context.dataset.data[context.dataIndex] > 15 ? "center" : "end",
+        align: (context) =>
+          context.dataset.data[context.dataIndex] > 15 ? "center" : "right",
+      },
+    },
+  },
+});
+
+const BriefImage = ChartBrifes.getUrl();
+
+
+    return(
+        <div className="col-6 border border-dark">
+        <h2>Etat d'avencement de brief :</h2>
+        {AllBriefs.map((value) => (
+          <div key={Math.random()}>
+            <li key={Math.random()}>{value.Nom_du_brief}</li>
+          </div>
+        ))}
+        <img style={{ width: 300 }} src={BriefImage}></img>
+      </div>
+    )
+}
+export default AvancementBriefs
---
+++ Realisation/frontEnd-V2/src/components/DashbordPage/AvancementGroupe.jsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import QuickChart from "quickchart-js";
+
+import React from "react";
+import { useEffect,useState   } from "react";
+import Cookies from "universal-cookie";
+
+
+function AvancementGroupe (){
+    
+    const [Pourcentage, setPourcentage] = useState([]);
+    const cookies = new Cookies();
+    
+    useEffect(() => {
+    let idFormateur = cookies.get("FormateurID");
+
+    const avancement = async () => {
+        await axios.get("http://localhost:8000/api/OneGroupe/" + idFormateur)
+          .then((res) => {
+            
+            setPourcentage(res.data[3].toFixed(0))
+            cookies.set("GroupeID", res.data[0].idGroupe);
+          });
+
+        };
+        avancement()   
+   
+}, []);
+
+
+    const myChart = new QuickChart();
+
+    myChart.setConfig({
+      type: "progressBar",
+      data: {
+        datasets: [
+          {
+            data: [Pourcentage],
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          datalabels: {
+            font: {
+              size: 30,
+            },
+            color: (context) =>
+              context.dataset.data[context.dataIndex] > 15 ? "#fff" : "#000",
+            anchor: (context) =>
+              context.dataset.data[context.dataIndex] > 15 ? "center" : "end",
+            align: (context) =>
+              context.dataset.data[context.dataIndex] > 15 ? "center" : "right",
+          },
+        },
+      },
+    });
+  
+    const chartImagee = myChart.getUrl();
+
+    return(
+
+        <div className="col-6 border border-dark ">
+        <h2>Etat d' avancement du groupe:</h2>
+        <img style={{ width: 300 }} src={chartImagee}></img>
+      </div>
+    )
+}
+
+export default AvancementGroupe
